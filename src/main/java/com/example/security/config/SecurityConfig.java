dcb01b7aa5f0ac7843a3d7abf789a36ed646aca6
minu <made_power@naver.com>
2023-05-29 22:23:27 +0900

    SecurityConfig 설정
    http 설정값에 관하여 어떤 것들이 있는지 확인하였다. 이 값들은 5.7 버전 이전에 사용할 수 있었던 값들이다. 현재 FormLogin은 사용할 수 없다.

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,22 +1,36 @@
 package com.example.security.config;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
                 .anyRequest().authenticated();
-        return http.build();
+        http
+                .formLogin()
+                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc");
 
     }
 }
--- src/main/java/com/example/security/controller/SecurityController.java
+++ src/main/java/com/example/security/controller/SecurityController.java
@@ -10,4 +10,8 @@ public class SecurityController {
         return "home";
     }
 
+    @GetMapping("loginPage")
+    public String loginPage(){
+        return "loginPage";
+    }
 }
